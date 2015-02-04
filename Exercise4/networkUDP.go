36045dc433443750acd8f0fbde55240e83a7cf9e
olemsk <olemartinskafsaa@hotmail.com>
2015-02-04 11:49:11 +0100

    changes

--- Exercise4/networkUDP.go
+++ Exercise4/networkUDP.go
@@ -1,10 +1,12 @@
-// go run udpRecieve.go
+// go run networkUDP.go
 package main
 import (. "fmt" // Using '.' to avoid prefixing functions with their package names
 		// This is probably not a good idea for large projects...
 	"runtime"
 	"time"
 	."net"
+	"bufio"
+	"os"
 )
 
 func checkError(err error) {
@@ -15,56 +17,79 @@ func checkError(err error) {
 	
 }
 
-func read() {
+
+//func broadcast(data []byte, conn *UDPConn) { // "129.241.187.255:
+//	udpAddr, err := ResolveUDPAddr("udp", "129.241.187.255:20001")
+//	checkError(err)
+//	conn, err := DialUDP("udp", nil, udpAddr)
+//	checkError(err)
+//	for {
+//		conn.Write(data) // conn.Write([]byte("Hvem er der?\n"))
+//	
+//	}
+//}
+
+func listen() {
 	buffer := make([]byte, 1024)
-	udpAddr, err := ResolveUDPAddr("udp4", ":20001")
-	conn, err := ListenUDP("udp4", udpAddr)
+	udpAddr, err := ResolveUDPAddr("udp", ":31111")
+	conn, err := ListenUDP("udp", udpAddr)
 	checkError(err)
 	for {
+		Println("Hører")
 		n,err := conn.Read(buffer)
 		checkError(err)
 		Printf("Rcv %d bytes: %s\n",n, buffer)
-		
 	}	
 }
 
-func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
-	// recvSock = 
-	//buffer := make([]byte, 1024)
-	udpAddr, err := ResolveUDPAddr("udp4", "129.241.187.136:20001")
+
+func send(ip []byte) { // data []byte
+	udpAddr, err := ResolveUDPAddr("udp", string(ip[:21]))
 	checkError(err)
-	Println("1")
-	conn, err := DialUDP("udp4", nil, udpAddr)
+	conn, err := DialUDP("udp", nil, udpAddr)
 	checkError(err)
-	Println("2")
-	
-	go read()
-	
-	//Println(conn2)
-	//os.Exit(1)
 	for {
+		Println("SENDER")
 		//buffer = nil
 		time.Sleep(1000*time.Millisecond)
-		Println("Hei!")
 		
 		// WRITE
-		Println("Er du der server??")
-		_, err := conn.Write([]byte("Eg er her\n")) // \x00
-		//conn.Write([]byte("Eg er her\n"))
+		//Println("Er du der server??")
+		_, err := conn.Write([]byte("fetbmwazz\n")) // \x00
 		checkError(err)
+	}
+
+}
+
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does... 
+	//buffer := make([]byte, 1024)
+	reader := bufio.NewReader(os.Stdin)
+	Print("IP til mottaker:")
+	ip, _ := reader.ReadBytes('\n')
+	go send(ip)
+	go listen()
+
+	for {
 		
-				
-		// READ
-		//n,err := conn.Read(buffer)
-		//checkError(err)
-		//Printf("Rcv %d bytes: %s\n",n, buffer)
+		//Print(ip)
+		//Print("Melding:")
+		//msg, _ := reader.ReadString('\n')
 		
 
+
 	}
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 
 
 }
